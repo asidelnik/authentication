87c0d1557b5f8ffa6d0dcf0001e471d92f46e6b3
Amos <asidelnik@gmail.com>
2018-06-18 14:48:16 +0300

    Starting passport

---
+++ server.js
@@ -0,0 +1,48 @@
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const app = express();
+
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//Authentication middleware
+app.use(passport.initialize());
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log(`API running on localhost:${port}`));
+
+passport.use(new LocalStrategy(function (username, password, done) {
+    if ((username === "john") && (password === "password")) {
+        return done(null, { username: username, id: 1 });
+    } else {
+        return done(null, false);
+    }
+}));
+
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/Login.html'));
+});
+
+app.post('/login', function () {
+});
+
+
